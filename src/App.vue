d3501e4a7378921f08abbc476ffe21312e3c5fb1
Hans Pärna <hans.paerna@native-instruments.com>
2026-08-03 21:39:56 +0200

    add basic layout

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,36 @@
 
 <template>
   <header>
-    <h1>Hans Pärna</h1>
-    <h4>Senior Backend Engineer & DevOps</h4>
   </header>
 
   <main>
+    <div class="box">
+      <div class="container">
+        <div class="logo">
+          <h1>Hans Pärna</h1>
+          <h4>Senior Backend Engineer & DevOps</h4>
+        </div>
+        <div class="contacts">
+          <p>Phone: +49 888 88-888-88</p>
+          <p>E-mail: hans.paerna@tsal.al</p>
+          <p>LinkedIn: link</p>
+          <p>Github: link</p>
+        </div>
+        <div class="topmenu">
+          <div class="grow text-center">
+            /about
+          </div>
+          <div class="grow text-center">
+            /projects
+          </div>
+          <div class="grow text-center">
+            /cv
+          </div>
+        </div>
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+    </div>
   </main>
 </template>

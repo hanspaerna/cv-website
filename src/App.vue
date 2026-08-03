b55afb45d6346c46018ff1148e0626032b1bc052
Hans Pärna <hans.paerna@native-instruments.com>
2026-08-03 19:22:47 +0200

    add michroma font, install tailwind

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,10 @@
 
 <template>
   <header>
-    <div class="wrapper">
-      ...
-    </div>
+    <h1>Hans Pärna</h1>
+    <h4>Senior Backend Engineer & DevOps</h4>
   </header>
 
   <main>
-    ...
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>

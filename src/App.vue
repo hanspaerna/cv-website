6aa26e67ea78f1da1242ca088a31b0991e18fc48
Hans Pärna <hans.paerna@native-instruments.com>
2026-08-03 18:51:15 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      ...
+    </div>
+  </header>
+
+  <main>
+    ...
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>

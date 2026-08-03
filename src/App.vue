d92ce1b16e20f096b6e696f2da2b6b2e8225924a
Hans Pärna <hans.paerna@native-instruments.com>
2026-08-03 23:48:43 +0200

    add box shadows and paddings

--- src/App.vue
+++ src/App.vue
@@ -12,24 +12,24 @@
           <h1>Hans Pärna</h1>
           <h4>Senior Backend Engineer & DevOps</h4>
         </div>
-        <div class="contacts">
-          <p>Phone: +49 888 88-888-88</p>
-          <p>E-mail: hans.paerna@tsal.al</p>
-          <p>LinkedIn: link</p>
-          <p>Github: link</p>
+        <div class="contacts shadow-deus shadow-primary inset-shadow-deus inset-shadow-primary">
+          <p>+49 888 88-888-88</p>
+          <p>hans.paerna@tsal.al</p>
+          <p>LinkedIn</p>
+          <p>Github</p>
         </div>
-        <div class="topmenu">
+        <div class="topmenu shadow-deus shadow-primary inset-shadow-deus inset-shadow-primary">
           <div class="grow text-center">
-            /about
+            /> about
           </div>
           <div class="grow text-center">
-            /projects
+            /> projects
           </div>
           <div class="grow text-center">
-            /cv
+            /> cv
           </div>
         </div>
-        <div class="content">
+        <div class="content shadow-deus shadow-primary inset-shadow-deus inset-shadow-primary">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
       </div>
